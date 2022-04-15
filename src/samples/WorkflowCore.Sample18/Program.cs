bd81f892154944080b1c4bfee056dbc3d1fd68df
Miguel Gonzalez <mig.dev.gonzalez@outlook.com>
2022-04-15 14:05:31 -0400

    included SqpServerWorkflowCore.Persistence.SqlServer.csproj reference

--- src/samples/WorkflowCore.Sample18/Program.cs
+++ src/samples/WorkflowCore.Sample18/Program.cs
@@ -37,8 +37,15 @@ namespace WorkflowCore.Sample18
             //setup dependency injection
             IServiceCollection services = new ServiceCollection();
             //services.AddWorkflow();
-            services.AddWorkflow(x => x.UseMongoDB(@"mongodb://localhost:27017", "workflow"));
+            //services.AddWorkflow(x => x.UseMongoDB(@"mongodb://localhost:27017", "workflow"));
+
+            //Note: for SqlServer the Server=.  does not seem to work when running on localhost,
+            //so replace the . with (localdb)\MSSQLLocalDB and it should work
             //services.AddWorkflow(x => x.UseSqlServer(@"Server=.;Database=WorkflowCore;Trusted_Connection=True;", true, true));
+            services.AddWorkflow(x => x.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=WorkflowCore2;Trusted_Connection=True;", true, true));
+            //services.AddWorkflow(x => x.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=WorkflowCore;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False", true, true));
+            //"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=WorkflowCore;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+            
             //services.AddWorkflow(x => x.UsePostgreSQL(@"Server=127.0.0.1;Port=5432;Database=workflow;User Id=postgres;", true, true));
             services.AddLogging(cfg => 
             {
